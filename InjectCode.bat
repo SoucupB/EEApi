b8372bd42c79b4a29cdf36e25325d243e267558f
SoucupB <bogdan.soucup@gmail.com>
2025-04-16 00:07:25 +0300

    Inject and build

--- InjectCode.bat
+++ InjectCode.bat
@@ -1,6 +1,10 @@
 @echo off
 setlocal enabledelayedexpansion
 set "pathTo=%cd%"
+
+cmd /c "InjectorBuild.bat"
+cmd /c "LibraryBuild.bat"
+
 set binaryPath=LibraryBinary
 
 set toExecute=InjectorBinaries\DllInjector32BitsExec.exe !pathTo!\!binaryPath!\Bots.dll
