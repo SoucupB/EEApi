4afeb5065148977e9ba66908509c371391405863
SoucupB <bogdan.soucup@gmail.com>
2026-04-01 00:52:52 +0300

    WIP

--- Build/Sources/EmpireEarth.cpp
+++ Build/Sources/EmpireEarth.cpp
@@ -87,11 +87,11 @@ extern "C" {
 }
 
 void addBotMethodsHooks() {
-  // builder_Definition((PVOID)0x1540CD, (PVOID)onUnitIteration);
-  // builder_Definition((PVOID)0x15321F, (PVOID)onFrame);
-  // builder_Definition((PVOID)0x13B8CF, (PVOID)onUnitDelete);
-  // builder_Definition((PVOID)0x1F5E09, (PVOID)onPlanePark);
-  // builder_Definition((PVOID)0x16B3C3, (PVOID)onUnitBuy);
+  builder_Definition((PVOID)0x1540CD, (PVOID)onUnitIteration);
+  builder_Definition((PVOID)0x15321F, (PVOID)onFrame);
+  builder_Definition((PVOID)0x13B8CF, (PVOID)onUnitDelete);
+  builder_Definition((PVOID)0x1F5E09, (PVOID)onPlanePark);
+  builder_Definition((PVOID)0x16B3C3, (PVOID)onUnitBuy);
 }
 
 extern "C"  __declspec(dllexport) void __cdecl someDllMain() {
