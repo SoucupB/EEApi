7070a17feacd04cf36fe81df621e199467b2bc50
SoucupB <bogdan.soucup@gmail.com>
2026-04-22 21:15:09 +0300

    WIP

--- Build/Headers/Unit.h
+++ Build/Headers/Unit.h
@@ -8,3 +8,5 @@ vector<Unit> unit_GetBuildings(int8_t player);
 vector<Unit> unit_GetUnits(int8_t player);
 int32_t unit_CurrentlyBuilding(Unit building);
 int8_t unit_IsBuilding(Unit unit);
+TilePoint unit_Tile_Position(Unit unit);
+uint16_t unit_GetPlaneID(Unit unit);
--- Build/Sources/Unit.cpp
+++ Build/Sources/Unit.cpp
@@ -1,5 +1,6 @@
 #include "Unit.h"
 #include "LibManager.h"
+#include "MapData.h"
 #include <unordered_map>
 
 unordered_map<PVOID, uint8_t> *eeTa_GetUnitPresence();
@@ -65,4 +66,17 @@ int8_t unit_IsBuilding(Unit unit) {
   size_t *unitMetaData = (size_t *)util_Pointer((PVOID)unit._payload, 0x2C, POINTER_TYPE);
   size_t *callerStruct = (size_t *)util_Pointer((PVOID)unitMetaData[0], 0xB8, POINTER_TYPE);
   return (size_t)callerStruct == (size_t)lib_BaseAddress() + 0x20FD9D;
+}
+
+TilePoint unit_Tile_Position(Unit unit) {
+  int32_t x = *(int32_t *)util_Pointer(eeTa_Unit_Reference(unit), 0x1C, INT32_T_TYPE);
+  int32_t y = *(int32_t *)util_Pointer(eeTa_Unit_Reference(unit), 0x20, INT32_T_TYPE);
+  return (TilePoint) {
+    .x = x,
+    .y = y
+  };
+}
+
+uint16_t unit_GetPlaneID(Unit unit) {
+  return map_Tile_GetPlaneID(unit_Tile_Position(unit));
 }
