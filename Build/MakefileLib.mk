b5da62a3b06cd7080ae622d78620df86b61fac5c
SoucupB <bogdan.soucup@gmail.com>
2026-05-13 21:11:15 +0300

    WIP

---
+++ Build/MakefileLib.mk
@@ -0,0 +1,18 @@
+CXX = g++
+CXXFLAGS = -std=gnu++11 -IHeaders/ -O2 -lPsapi -lgdi32 -luser32
+AR = ar rcs
+
+SRC_DIR = Sources
+OBJ_DIR = lib
+TARGET = Engine.a
+
+SOURCES = $(wildcard $(SRC_DIR)/*.cpp)
+OBJECTS = $(patsubst $(SRC_DIR)/%.cpp,$(OBJ_DIR)/%.o,$(SOURCES))
+
+all: $(TARGET)
+
+$(TARGET): $(OBJECTS)
+	$(AR) $(OBJ_DIR)/$(TARGET) $^
+
+$(OBJ_DIR)/%.o: $(SRC_DIR)/%.cpp
+	$(CXX) $(CXXFLAGS) -c $< -o $@
