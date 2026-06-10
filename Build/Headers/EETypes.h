4dc56e663990c469bdfcf33a728f0c1f38dc8414
SoucupB <bogdan.soucup@gmail.com>
2026-06-10 20:48:19 +0300

    Add eetypes comments

--- Build/Headers/EETypes.h
+++ Build/Headers/EETypes.h
@@ -6,15 +6,45 @@
 
 using namespace std;
 
+/*
+ * Checks if a unit type is from a specific class.
+ */
 uint8_t eeTypes_IsFromClass(UnitClassType unitClass, UnitType unitType);
+/*
+ * Checks if a unit type is a unit that can go through water (attacking/farming/transport boats)
+ */
 uint8_t eeTypes_IsWaterUnit(UnitType unitType);
+/*
+ * Checks if a unit type is a citizen (female/male)
+ */
 uint8_t eeTypes_IsCitizen(UnitType unitType);
+/*
+ * Checks if a unit type is a water transport boat (any epoch)
+ */
 uint8_t eeTypes_IsWaterTransport(UnitType unitType);
+/*
+ * Checks if a unit type is a transport, both water and flying unit.
+ */
 uint8_t eeTypes_IsTransport(UnitType unitType);
+/*
+ * Checks if a unit type is a ground unit but buildings are not included.
+ */
 uint8_t eeTypes_IsGroundUnit(UnitType unitType);
+/*
+ * Checks if a unit type is a fish boat
+ */
 uint8_t eeTypes_IsFishBoat(UnitType unitType);
+/*
+ * Checks if a unit type is a priest
+ */
 uint8_t eeTypes_IsPriest(UnitType type);
+/*
+ * Checks if a unit type is a plane, helicopter or any other type which flies.
+ */
 uint8_t eeTypes_IsAirUnit(UnitType unitType);
+/*
+ * Checks if a unit type is either a capitol or town center.
+ */
 uint8_t eeTypes_CanProduceWorkers(UnitType type);
 PVOID eeTypes_GetTemplate(UnitType typeIndex);
 char *eeTypes_Name(UnitType type);
